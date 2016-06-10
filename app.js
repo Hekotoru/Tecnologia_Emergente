f6d088543e54d3459a5866355edd280203675128
Hector Aristy <rafelito266@gmail.com>
2016-06-09 20:11:54 -0400

    Actualizacion del 9 de junio.

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ var app = express();
 
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
-
+var world = require('./public/js/server_world');
 
 app.get('/', function(req, res){
     res.sendFile(__dirname + '/index.html');
